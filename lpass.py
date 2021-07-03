d97fd4905dddf55a93494d3b552aaef232621f7d
Arav <56185632+arav06@users.noreply.github.com>
2021-07-03 19:03:59 +0530

    Add files via upload

---
+++ lpass.py
@@ -0,0 +1,206 @@
+from cryptography.fernet import Fernet as frt
+import getpass as gp
+import string
+import random
+import pandas as pd
+from csv import writer
+import csv
+from csv import DictWriter
+import colorama
+from colorama import Fore
+import os
+
+banner = """
+
+██╗░░░░░██████╗░░█████╗░░██████╗░██████╗
+██║░░░░░██╔══██╗██╔══██╗██╔════╝██╔════╝
+██║░░░░░██████╔╝███████║╚█████╗░╚█████╗░
+██║░░░░░██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗
+███████╗██║░░░░░██║░░██║██████╔╝██████╔╝
+╚══════╝╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░
+"""
+
+print(banner+"\n\n")
+print("Welcome to LPass, a command line password manager. \nType 'help' to get started\n")
+
+keyLocation = str(input("[+]Enter the location of the key file> "))
+keyInput = keyLocation.strip()
+user = gp.getuser()
+i = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=5))
+colorama.init(autoreset=True)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+try:
+      keyfile = open(keyInput,'rb')
+      key = keyfile.read()
+      frtObj = frt(key)
+
+
+      with open("LPass.csv",'rb') as encryptedFile:
+            encryptedData = encryptedFile.read()
+
+      decryptedData = frtObj.decrypt(encryptedData)
+
+      with open('LPass.csv','wb') as decryptedFile:
+            decryptedFile.write(decryptedData)
+      with open('LPass.csv', 'rb') as f:
+            toEncrypt = f.read()
+
+      encrypted_data = frtObj.encrypt(toEncrypt)
+
+      with open('LPass.csv', 'wb') as encrypted_file:
+            encrypted_file.write(encrypted_data)
+
+
+
+except:
+      print(Fore.RED+"\n[+]An error has occured")
+      exit()
+print(Fore.GREEN+f"\n[+]Hey {gp.getuser()}!!")
+while True:
+
+
+      try:
+      
+            print("\n")
+            cmd = str(input(f"{user}@lpass-{i}$ "))
+
+
+            if cmd == "exit":
+                  print(Fore.GREEN+"\n[+]Bye :)")
+                  break
+
+            elif cmd == "help":
+                  helpmenu = """
+                  
+█░░ █▀█ ▄▀█ █▀ █▀   █░█ █▀▀ █░░ █▀█   █▀▄▀█ █▀▀ █▄░█ █░█
+█▄▄ █▀▀ █▀█ ▄█ ▄█   █▀█ ██▄ █▄▄ █▀▀   █░▀░█ ██▄ █░▀█ █▄█
+
+                  """
+                  print(f"\n{helpmenu} \n\n")
+                  print(Fore.GREEN+"help - Prints the manual\n")
+                  print(Fore.GREEN+"exit - Exit the program\n") 
+                  print(Fore.GREEN+"view - Displays the vault \n")    
+                  print(Fore.GREEN+"add - Adds title,website,username and password which is provided, to the vault\n")
+                  print(Fore.GREEN+"generate - Generates a secure password")
+                  print(Fore.GREEN+"clear - Clears the screen")
+
+
+
+            elif cmd == "view":
+                  print("\n")
+                  print(Fore.GREEN+"Your Vault: \n")
+                  try:
+                        keyfile = open(keyInput,'rb')
+                        key = keyfile.read()
+                        frtObj = frt(key)
+
+
+                        with open("LPass.csv",'rb') as encryptedFile:
+                              encryptedData = encryptedFile.read()
+
+                        decryptedData = frtObj.decrypt(encryptedData)
+
+                        with open('LPass.csv','wb') as decryptedFile:
+                              decryptedFile.write(decryptedData)
+                        data = pd.read_csv("LPass.csv") 
+                        print(data)
+
+                  except:
+                        data = pd.read_csv("LPass.csv") 
+                        print(data)
+                  with open('LPass.csv', 'rb') as f:
+                        toEncrypt = f.read()
+
+                  encrypted_data = frtObj.encrypt(toEncrypt)
+
+                  with open('LPass.csv', 'wb') as encrypted_file:
+                        encrypted_file.write(encrypted_data)
+
+            elif cmd == "add":
+                  print(Fore.GREEN+"[+]Add login credentials to your vault!\n\n")
+                  name = str(input("Enter the name\n>"))
+                  print("\n")
+                  website = str(input("Enter the website\n>"))
+                  print("\n")
+                  username = str(input("Enter the username\n>"))
+                  print("\n")
+                  print("For security, you will not be able to see the password you type\n")
+                  password = str(gp.getpass(prompt='Enter the password\n> '))
+                  print("\n")
+                  print(Fore.GREEN+f"Name:{name}\nWebsite:{website}\nUsername:{username}\nPassword:{password}")
+                  confirm = str(input("\n[+]Are you sure you wish to the following login credentials(y or n): "))
+                  
+                  if confirm.lower() == "n":
+                        print(Fore.RED+f"\n[+]Not saving login credentials for {name}")
+
+                  elif confirm.lower() == "y":
+                        try:
+                              keyfile = open(keyInput,'rb')
+                              key = keyfile.read()
+                              frtObj = frt(key)
+
+
+                              with open("LPass.csv",'rb') as encryptedFile:
+                                    encryptedData = encryptedFile.read()
+
+                              decryptedData = frtObj.decrypt(encryptedData)
+
+                              with open('LPass.csv','wb') as decryptedFile:
+                                    decryptedFile.write(decryptedData)
+                              creds=[name,website,username,password]
+                              with open('LPass.csv', 'a') as f2:
+                              
+                                    f2.write("\n")
+                                    wo = writer(f2)
+
+                                    wo.writerow(creds)
+
+                                    f2.close()
+
+                        except:
+                              creds=[name,website,username,password]
+                              with open('LPass.csv', 'a') as f2:
+                              
+
+                                    wo = writer(f2)
+
+                                    wo.writerow(creds)
+
+                                    f2.close()
+                        with open('LPass.csv', 'rb') as f:
+                              toEncrypt = f.read()
+
+                        encrypted_data = frtObj.encrypt(toEncrypt)
+
+                        with open('LPass.csv', 'wb') as encrypted_file:
+                              encrypted_file.write(encrypted_data)
+                        print(Fore.GREEN+f"\n[+]Login credentials for {name} have been saved")
+                  else:
+                        print(Fore.RED+"\n[+]Invalid Input")
+
+
+            elif cmd == "":
+                  pass
+            elif cmd == "clear":
+                  if os.name == 'nt':
+                        os.system('cls')
+  
+    
+                  else:
+                        os.system('clear')
+
+            elif cmd == "generate":
+                  print("\n")
+                  length = int(input("Enter the length> "))
+                  password = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=length))
+                  print(Fore.GREEN+f"\n[+]Your secure password is:")
+                  print(f"\n\n{password}")
+
+            else:
+                  print(Fore.RED+"\n[+]Invalid Command")
+      except KeyboardInterrupt:
+            pass
---
+++ setup.py
@@ -0,0 +1,43 @@
+from cryptography.fernet import Fernet as frt
+import getpass as gp
+import os
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+print(Fore.LIGHTGREEN_EX+"\n[+]Welcome to LPass, a local command line password manager \n\n")
+filename = f'LPass-{gp.getuser()}.key'
+print(Fore.GREEN+"The following modules will be installed")
+print("\n\n1)pandas\n2)cryptography\n3)colorama\n\n")
+print(Fore.GREEN+"The following files will be created\n")
+print(f"1)LPass.csv(Vault where details are stored in an encrypted format\n2){filename}(The key to unlock the vault)\n\n")
+ask = str(input("Do you wish to proceed with the installation(y or n)?\n"))
+answer = ask.lower()
+if answer == "y":
+      print(Fore.GREEN+"\n[+]Generating secret key. Store this file in a safe location as this is used to open the password vault ")
+      secretKey = frt.generate_key()
+      keyFile = open(filename,'wb')
+      keyFile.write(secretKey)
+      print(Fore.LIGHTGREEN_EX+f"\n[+]Secret key has been generated and is stored in {filename}\n")
+      print(Fore.GREEN+"[+]Installing pandas,cryptography and colorama\n")
+      os.system("pip install pandas")
+      os.system("pip install cryptography")
+      os.system("pip install colorama")
+      print(Fore.LIGHTGREEN_EX+"\n[+]Modules have been installed\n")
+      print(Fore.GREEN+"[+]Creating Vault")
+      vault = open('LPass.csv','wb')
+      fr = frt(secretKey)
+      r = f"""
+      Name,Website,Username,Password
+      LPass,N/A,{gp.getuser()},{secretKey}
+      """
+      encrypted = fr.encrypt(r.encode())
+      with open('LPass.csv', 'wb') as encrypted_file:
+            encrypted_file.write(encrypted)
+      print(Fore.LIGHTGREEN_EX+"\n[+]Vault has been created\n\n")
+      print(Fore.LIGHTGREEN_EX+"[+]Setup is completed!")
+elif answer == "n":
+      print(Fore.RED+"\n\n[+]Not proceeding with the setup")
+      exit()
+else:
+      print(Fore.RED+"\n\n[+]Invalid input, not proceeding with installation")
+      exit()
