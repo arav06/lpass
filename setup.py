75151366175d96a4c699ab1fb21a2ddb355f62db
Arav <56185632+arav06@users.noreply.github.com>
2021-07-04 11:23:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from cryptography.fernet import Fernet as frt
 import getpass as gp
-import os
 import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 print(Fore.LIGHTGREEN_EX+"\n[+]Welcome to LPass, a local command line password manager \n\n")
 filename = f'LPass-{gp.getuser()}.key'
-print(Fore.GREEN+"The following modules will be installed")
-print("\n\n1)pandas\n2)cryptography\n\n")
 print(Fore.GREEN+"The following files will be created\n")
 print(f"1)LPass.csv(Vault where details are stored in an encrypted format\n2){filename}(The key to unlock the vault)\n\n")
 ask = str(input("Do you wish to proceed with the installation(y or n)?\n"))
@@ -18,16 +15,12 @@ if answer == "y":
       keyFile = open(filename,'wb')
       keyFile.write(secretKey)
       print(Fore.LIGHTGREEN_EX+f"\n[+]Secret key has been generated and is stored in {filename}\n")
-      print(Fore.GREEN+"[+]Installing pandas,cryptography and colorama\n")
-      os.system("pip install pandas")
-      os.system("pip install cryptography")
-      print(Fore.LIGHTGREEN_EX+"\n[+]Modules have been installed\n")
       print(Fore.GREEN+"[+]Creating Vault")
       vault = open('LPass.csv','wb')
       fr = frt(secretKey)
       r = f"""
-      Name,Website,Username,Password
-      LPass,N/A,{gp.getuser()},{secretKey}
+      Name,Username,Password
+      LPass,{gp.getuser()},{secretKey}
       """
       encrypted = fr.encrypt(r.encode())
       with open('LPass.csv', 'wb') as encrypted_file:
