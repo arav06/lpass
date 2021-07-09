529f3f5d2c2edb9ba5bec2510c8e5b91d93180fa
Arav <56185632+arav06@users.noreply.github.com>
2021-07-09 20:01:27 +0530

    Update lpass.py

--- lpass.py
+++ lpass.py
@@ -10,6 +10,7 @@ import colorama
 from colorama import Fore
 import os
 
+
 banner = """
 
 ██╗░░░░░██████╗░░█████╗░░██████╗░██████╗
@@ -22,7 +23,7 @@ banner = """
 
 print(banner+"\n\n")
 print("Welcome to LPass, a command line password manager. \n")
-
+vault = "LPass.csv"
 keyLocation = str(input("[+]Enter the location of the key file> "))
 keyInput = keyLocation.strip()
 user = gp.getuser()
@@ -33,31 +34,127 @@ pd.set_option('display.max_columns', None)
 #pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
+
+def decrypt(vlt):
+      keyfile = open(keyInput,'rb')
+      key = keyfile.read()
+      frtObj = frt(key)
+
+
+      with open(vlt,'rb') as encryptedFile:
+            encryptedData = encryptedFile.read()
+
+      decryptedData = frtObj.decrypt(encryptedData)
+
+      with open(vlt,'wb') as decryptedFile:
+            decryptedFile.write(decryptedData)
+
+
+def encrypt(vlt):
+      with open(vlt, 'rb') as f:
+            toEncrypt = f.read()
+
+      encrypted_data = frtObj.encrypt(toEncrypt)
+      
+      with open(vlt, 'wb') as encrypted_file:
+            encrypted_file.write(encrypted_data)
+
+def addCreds(name,website,username,password,confirm,vlt):
+      if confirm.lower() == "n":
+            print(Fore.RED+f"\n[+]Not adding login credentials for {name}")
+
+      elif confirm.lower() == "y":
+            try:
+                  keyfile = open(keyInput,'rb')
+                  key = keyfile.read()
+                  frtObj = frt(key)
+
+
+                  decrypt(vault)
+                  creds=[name,website,username,password]
+                  with open(vlt, 'a') as f2:
+                  
+                        f2.write("\n")
+                        wo = writer(f2)
+
+                        wo.writerow(creds)
+
+                        f2.close()
+
+            except:
+                  creds=[name,website,username,password]
+                  with open(vlt, 'a') as f2:
+                  
+
+                        wo = writer(f2)
+
+                        wo.writerow(creds)
+
+                        f2.close()
+            encrypt(vault)
+            print(Fore.GREEN+f"\n[+]Login credentials for {name} have been added")
+      else:
+            print(Fore.RED+f"\n[+]Invalid Input, not adding credentials for {name}")
+
+def viewVault(vlt):
+      print("\n")
+      print(Fore.GREEN+"[+]Your Vault: \n")
+      try:
+            keyfile = open(keyInput,'rb')
+            key = keyfile.read()
+            frtObj = frt(key)
+
+
+            decrypt(vault)
+            data = pd.read_csv(vlt) 
+            print(data)
+
+      except:
+            data = pd.read_csv(vlt) 
+            print(data)
+      encrypt(vault)
+
+
+def helpMenu():
+      helpmenu = """
+      
+      █░░ █▀█ ▄▀█ █▀ █▀   █░█ █▀▀ █░░ █▀█   █▀▄▀█ █▀▀ █▄░█ █░█
+      █▄▄ █▀▀ █▀█ ▄█ ▄█   █▀█ ██▄ █▄▄ █▀▀   █░▀░█ ██▄ █░▀█ █▄█
+
+      """
+      print(f"\n{helpmenu} \n\n")
+      print(Fore.GREEN+"help - Prints the manual\n")
+      print(Fore.GREEN+"exit - Exit the program\n") 
+      print(Fore.GREEN+"view - Displays the vault \n")    
+      print(Fore.GREEN+"add - Adds title,website,username and password which is provided, to the vault\n")
+      print(Fore.GREEN+"generate - Generates a secure password\n")
+      print(Fore.GREEN+"clear - Clears the screen")
+
+
+def passGen():
+      print(Fore.GREEN+"[+]Generate a secure password\n\n")
+      try:
+            length = int(input("Enter the length\n> "))
+            
+            password = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=length))
+            print(Fore.GREEN+f"\n[+]Your secure password is:")
+            print(f"\n\n{password}")
+      except :
+            print(Fore.RED+"\n[+]Invalid Input")
+
+
 try:
       keyfile = open(keyInput,'rb')
       key = keyfile.read()
       frtObj = frt(key)
 
 
-      with open("LPass.csv",'rb') as encryptedFile:
-            encryptedData = encryptedFile.read()
-
-      decryptedData = frtObj.decrypt(encryptedData)
-
-      with open('LPass.csv','wb') as decryptedFile:
-            decryptedFile.write(decryptedData)
-      with open('LPass.csv', 'rb') as f:
-            toEncrypt = f.read()
-
-      encrypted_data = frtObj.encrypt(toEncrypt)
-
-      with open('LPass.csv', 'wb') as encrypted_file:
-            encrypted_file.write(encrypted_data)
-
+      decrypt(vault)
+      encrypt(vault)
 
 
 except:
-      print(Fore.RED+"\n[+]An error has occured :(")
+      print(Fore.RED+"\n[+]An error has occured")
       exit()
 print(Fore.GREEN+f"\n[+]Hey {gp.getuser()}, type 'help' to get started!")
 while True:
@@ -74,51 +171,12 @@ while True:
                   break
 
             elif cmd == "help":
-                  helpmenu = """
-                  
-█░░ █▀█ ▄▀█ █▀ █▀   █░█ █▀▀ █░░ █▀█   █▀▄▀█ █▀▀ █▄░█ █░█
-█▄▄ █▀▀ █▀█ ▄█ ▄█   █▀█ ██▄ █▄▄ █▀▀   █░▀░█ ██▄ █░▀█ █▄█
-
-                  """
-                  print(f"\n{helpmenu} \n\n")
-                  print(Fore.GREEN+"help - Prints the manual\n")
-                  print(Fore.GREEN+"exit - Exit the program\n") 
-                  print(Fore.GREEN+"view - Displays the vault \n")    
-                  print(Fore.GREEN+"add - Adds title,website,username and password which is provided, to the vault\n")
-                  print(Fore.GREEN+"generate - Generates a secure password\n")
-                  print(Fore.GREEN+"clear - Clears the screen")
+                  helpMenu()
 
 
 
             elif cmd == "view":
-                  print("\n")
-                  print(Fore.GREEN+"[+]Your Vault: \n")
-                  try:
-                        keyfile = open(keyInput,'rb')
-                        key = keyfile.read()
-                        frtObj = frt(key)
-
-
-                        with open("LPass.csv",'rb') as encryptedFile:
-                              encryptedData = encryptedFile.read()
-
-                        decryptedData = frtObj.decrypt(encryptedData)
-
-                        with open('LPass.csv','wb') as decryptedFile:
-                              decryptedFile.write(decryptedData)
-                        data = pd.read_csv("LPass.csv") 
-                        print(data)
-
-                  except:
-                        data = pd.read_csv("LPass.csv") 
-                        print(data)
-                  with open('LPass.csv', 'rb') as f:
-                        toEncrypt = f.read()
-
-                  encrypted_data = frtObj.encrypt(toEncrypt)
-
-                  with open('LPass.csv', 'wb') as encrypted_file:
-                        encrypted_file.write(encrypted_data)
+                  viewVault(vault)
 
             elif cmd == "add":
                   print(Fore.GREEN+"[+]Add login credentials to your vault!\n\n")
@@ -133,58 +191,14 @@ while True:
                   print("\n")
                   print(Fore.GREEN+f"Name:{name}\nWebsite:{website}\nUsername:{username}\nPassword:{password}")
                   confirm = str(input("\n[+]Are you sure you wish to add the following login credentials(y or n): "))
+                  addCreds(name,website,username,password,confirm,vault)
                   
-                  if confirm.lower() == "n":
-                        print(Fore.RED+f"\n[+]Not adding login credentials for {name}")
 
-                  elif confirm.lower() == "y":
-                        try:
-                              keyfile = open(keyInput,'rb')
-                              key = keyfile.read()
-                              frtObj = frt(key)
-
-
-                              with open("LPass.csv",'rb') as encryptedFile:
-                                    encryptedData = encryptedFile.read()
-
-                              decryptedData = frtObj.decrypt(encryptedData)
-
-                              with open('LPass.csv','wb') as decryptedFile:
-                                    decryptedFile.write(decryptedData)
-                              creds=[name,website,username,password]
-                              with open('LPass.csv', 'a') as f2:
-                              
-                                    f2.write("\n")
-                                    wo = writer(f2)
-
-                                    wo.writerow(creds)
-
-                                    f2.close()
-
-                        except:
-                              creds=[name,website,username,password]
-                              with open('LPass.csv', 'a') as f2:
-                              
-
-                                    wo = writer(f2)
-
-                                    wo.writerow(creds)
-
-                                    f2.close()
-                        with open('LPass.csv', 'rb') as f:
-                              toEncrypt = f.read()
-
-                        encrypted_data = frtObj.encrypt(toEncrypt)
-
-                        with open('LPass.csv', 'wb') as encrypted_file:
-                              encrypted_file.write(encrypted_data)
-                        print(Fore.GREEN+f"\n[+]Login credentials for {name} have been added")
-                  else:
-                        print(Fore.RED+f"\n[+]Invalid Input, not adding credentials for {name}")
 
 
             elif cmd == "":
                   pass
+                  
             elif cmd == "clear":
                   if os.name == 'nt':
                         os.system('cls')
@@ -194,18 +208,11 @@ while True:
                         os.system('clear')
 
             elif cmd == "generate":
-                  print(Fore.GREEN+"[+]Generate a secure password\n\n")
-                  try:
-                        length = int(input("Enter the length\n> "))
-                        
-                        password = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=length))
-                        print(Fore.GREEN+f"\n[+]Your secure password is:")
-                        print(f"\n\n{password}")
-                  except :
-                        print(Fore.RED+"\n[+]Invalid Input")
+                  passGen()
 
 
             else:
                   print(Fore.RED+"\n[+]Invalid Command")
+                  
       except KeyboardInterrupt:
             pass
